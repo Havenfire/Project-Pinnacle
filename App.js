1aadf6a48413949fbce0a29ab8a55ae6e179a386
jakebrown11235 <jakebrown5603@gmail.com>
2023-02-13 17:40:26 -0500

    Camera testing

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import * as Permissions from 'expo-permissions';
 
-export default function App() {
+const TakePhoto = () => {
+  const [photo, setPhoto] = useState(null);
+
+  const takePhoto = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === 'granted') {
+      const result = await ImagePicker.launchCameraAsync({
+        allowsEditing: true,
+        aspect: [4, 3],
+      });
+
+      if (!result.cancelled) {
+        setPhoto(result.uri);
+      }
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      {photo && <Image source={{ uri: photo }} style={{ width: 200, height: 200 }} />}
+      <Button title="Take Photo" onPress={takePhoto} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default TakePhoto;
