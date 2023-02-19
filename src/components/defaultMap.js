14d8fc3cc7a0476739c4e03918fdaf1373a6a980
morningtea7 <2528308387@qq.com>
2023-02-19 01:02:46 -0500

    Add a pin at anywhere on map

--- src/components/defaultMap.js
+++ src/components/defaultMap.js
@@ -1,62 +1,173 @@
-import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-import * as Location from 'expo-location';
-import MapView, { Marker } from 'react-native-maps';
+import React, { Component } from "react";
+import { Text, View, StyleSheet, Button, StatusBar } from "react-native";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+import Dialog from "react-native-dialog";
 
 export default class DefaultMap extends Component {
     constructor(props) {
-        super(props)
+        super(props);
 
         this.state = {
-            mapRegion: { latitude: 33.797187, longitude: -84.322187, latitudeDelta: 0.1, longitudeDelta: 0.05 },
+            mapRegion: {
+                latitude: 33.797187,
+                longitude: -84.322187,
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.05,
+            },
             locationResult: null,
-            location: { coords: { latitude: 33.797187, longitude: -84.322187, accuracy: 25 } },
+            location: {
+                coords: {
+                    latitude: 33.797187,
+                    longitude: -84.322187,
+                    accuracy: 25,
+                },
+            },
+            pins: [],
+            showDialog: false,
+            tempTitle: null,
+            tempDescription: null,
+            tempCoordinate: null,
         };
     }
 
+    addPin(coordinate, title, description, image) {
+        if (coordinate && title && description) {
+            this.state.pins = this.state.pins.concat({
+                coordinate: {
+                    latitude: coordinate.latitude,
+                    longitude: coordinate.longitude,
+                },
+                title: title,
+                description: description,
+                image: image ? image : undefined,
+            });
+        }
+        return this.state.pins;
+    }
+
     componentDidMount() {
+        StatusBar.setHidden(true);
         this._getLocationAsync();
     }
 
-    _handleMapRegionChange = mapRegion => {
+    _handleMapRegionChange = (mapRegion) => {
         this.setState({ mapRegion });
     };
 
     _getLocationAsync = async () => {
         let { status } = await Location.requestForegroundPermissionsAsync();
-        if (status !== 'granted') {
+        if (status !== "granted") {
             this.setState({
-                locationResult: 'Permission to access location was denied',
+                locationResult: "Permission to access location was denied",
                 location,
             });
         }
 
         let location = await Location.getCurrentPositionAsync({});
         this.setState({ locationResult: JSON.stringify(location), location });
+        // this.addPin(
+        //     this.state.location.coords,
+        //     "I am here",
+        //     "This is my current location",
+        //     null
+        // );
+    };
+
+    onPressAddPin = async () => {
+        this.onPressDismissDialog();
+        if (this.state.tempTitle && this.state.tempDescription) {
+            if (this.state.tempCoordinate) {
+                this.addPin(
+                    this.state.tempCoordinate,
+                    this.state.tempTitle,
+                    this.state.tempDescription,
+                    null
+                );
+            } else {
+                await this._getLocationAsync();
+                this.addPin(
+                    this.state.location.coords,
+                    this.state.tempTitle,
+                    this.state.tempDescription,
+                    null
+                );
+            }
+        }
+        this.setState({
+            tempTitle: null,
+            tempDescription: null,
+            tempCoordinate: null,
+        });
+    };
+
+    onPressShowDialog = () => {
+        this.setState({ showDialog: true });
+    };
+
+    onPressDismissDialog = () => {
+        this.setState({ showDialog: false });
     };
 
     render() {
         return (
-            <View style = { styles.container }>
+            <View style={styles.container}>
                 <MapView
-                        style ={ styles.map }
-                        region = {{
-                            latitude: this.state.location.coords.latitude,
-                            longitude: this.state.location.coords.longitude,
-                            latitudeDelta: this.state.location.coords.accuracy * 0.001,
-                            longitudeDelta: this.state.location.coords.accuracy * 0.0005
-                        }}
-                        onRegionChange = { this._handleMapRegionChange }
-                    >
-                    <Marker
-                        coordinate = { this.state.location.coords }
-                        title = "My Location"
-                        description = "This is my current location"
-                    />
-                    <Text style = { styles.text }>
-                        Location Response: {this.state.locationResult}
+                    style={styles.map}
+                    region={{
+                        latitude: this.state.location.coords.latitude,
+                        longitude: this.state.location.coords.longitude,
+                        latitudeDelta:
+                            this.state.location.coords.accuracy * 0.001,
+                        longitudeDelta:
+                            this.state.location.coords.accuracy * 0.0005,
+                    }}
+                    onRegionChange={this._handleMapRegionChange}
+                    onPress={(e) =>
+                        this.setState({
+                            showDialog: true,
+                            tempCoordinate: e.nativeEvent.coordinate,
+                        })
+                    }
+                    onMarkerPress={() => this.setState({ showDialog: false })}>
+                    {this.state.pins
+                        ? this.state.pins.map((pin) => (
+                              <Marker
+                                  coordinate={pin.coordinate}
+                                  title={pin.title}
+                                  description={pin.description}
+                                  image={pin.image}
+                              />
+                          ))
+                        : null}
+                    <Text style={styles.text}>
+                        List of Pins on Map: {JSON.stringify(this.state.pins)}
                     </Text>
+                    {/* <Button
+                        onPress={this.onPressShowDialog}
+                        title={"Add a Pin at current location"}
+                    /> */}
                 </MapView>
+                <Dialog.Container visible={this.state.showDialog}>
+                    <Dialog.Title>Create a Pin</Dialog.Title>
+                    <Dialog.Input
+                        label="Title"
+                        onChangeText={(title) =>
+                            this.setState({ tempTitle: title })
+                        }
+                    />
+                    <Dialog.Input
+                        label="Description"
+                        onChangeText={(description) =>
+                            this.setState({ tempDescription: description })
+                        }
+                    />
+                    <Dialog.Button
+                        label="Cancel"
+                        onPress={this.onPressDismissDialog}
+                    />
+                    <Dialog.Button label="OK" onPress={this.onPressAddPin} />
+                </Dialog.Container>
             </View>
         );
     }
@@ -64,20 +175,21 @@ export default class DefaultMap extends Component {
 
 const styles = StyleSheet.create({
     container: {
+        width: "100%",
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#ffffff',
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#ffffff",
     },
     text: {
         margin: 40,
         fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'auto',
-        color: '#34495e',
+        fontWeight: "bold",
+        textAlign: "auto",
+        color: "#34495e",
     },
     map: {
-        width: '100%',
-        height: '100%',
+        width: "100%",
+        height: "100%",
     },
 });
