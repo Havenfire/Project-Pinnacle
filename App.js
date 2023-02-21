38326864127d42ebed9734b40760a77ca1d4f76b
Havenfire <bsliu24@gmail.com>
2023-02-20 21:48:05 -0500

    first commit, found template

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
 import DefaultMap from './src/components/defaultMap';
 import Camera from './src/components/camera';
+import Profile from './src/components/profile'
 import { View } from 'react-native';
 
 export default function App() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <DefaultMap />
-            {/* <Camera /> */}
-        </View>
+
+        <Profile/>
     );
 }
