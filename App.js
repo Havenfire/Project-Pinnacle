9f99ca051d2385b2ffa84f49368ab70108bf07a4
Havenfire <bsliu24@gmail.com>
2023-02-20 23:08:27 -0500

    not compile, but downloaded react navigation

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import React, { useState } from 'react';
 import DefaultMap from './src/components/defaultMap';
 import Camera from './src/components/camera';
-import Profile from './src/components/profile'
+import Profile from './src/components/profile';
 import { View } from 'react-native';
+import GoogleAuth from './src/components/googleAuth';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
 export default function App() {
+    const username = 'JohnDoe';
+    const first_name = 'John';
+    const last_name = 'Doe';
+    const full_name = 'John Doe';
+    const email = 'johndoe@example.com';
+    const profile_pic = '../src/assets/profile-pic.png';
+
+    const Stack = createNativeStackNavigator();
+
+
     return (
 
-        <Profile/>
+        <NavigationContainer>
+            <Stack.Navigator>
+
+
+                {<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <GoogleAuth />
+                    <Profile
+                        profile_pic={profile_pic}
+                        first_name={first_name}
+                        last_name={last_name}
+                        full_name={full_name}
+                        username={username}
+                        email={email}
+                    />
+                </View>}
+
+            </Stack.Navigator>
+
+        </NavigationContainer>
+
+
+
+
+
+
+
+
     );
 }
--- src/components/profile.js
+++ src/components/profile.js
@@ -2,91 +2,75 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView } from "react-native";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 
-export default function App() {
+export default class Profile extends React.Component {
 
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            full_name: null,
+            first_name: null,
+            last_name: null,
+            username: null,
+            email: null,
+            profile_pic: null,
+        };
+    }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView showsVerticalScrollIndicator={false}>
-                <View style={styles.titleBar}>
-                    <Ionicons name="ios-arrow-back" size={24} color="#52575D"></Ionicons>
-                    <Ionicons name="md-more" size={24} color="#52575D"></Ionicons>
-                </View>
 
-                <View style={{ alignSelf: "center" }}>
-                    <View style={styles.profileImage}>
-                        <Image source={require("../assets/profile_profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
+    render() {
+        return (
+            <SafeAreaView style={styles.container}>
+                <ScrollView showsVerticalScrollIndicator={false}>
+                    <View style={styles.titleBar}>
+                        <Ionicons name="ios-arrow-back" size={24} color="#52575D"></Ionicons>
+                        <Ionicons name="md-more" size={24} color="#52575D"></Ionicons>
                     </View>
-                    <View style={styles.dm}>
-                        <MaterialIcons name="chat" size={18} color="#DFD8C8"></MaterialIcons>
-                    </View>
-                    <View style={styles.active}></View>
-                    <View style={styles.add}>
-                        <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
-                    </View>
-                </View>
 
-                <View style={styles.infoContainer}>
-                    <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>Julie</Text>
-                    <Text style={[styles.text, { color: "#AEB5BC", fontSize: 14 }]}>Photographer</Text>
-                </View>
-
-                <View style={styles.statsContainer}>
-                    <View style={styles.statsBox}>
-                        <Text style={[styles.text, { fontSize: 24 }]}>483</Text>
-                        <Text style={[styles.text, styles.subText]}>Posts</Text>
-                    </View>
-                    <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
-                        <Text style={[styles.text, { fontSize: 24 }]}>45,844</Text>
-                        <Text style={[styles.text, styles.subText]}>Followers</Text>
-                    </View>
-                    <View style={styles.statsBox}>
-                        <Text style={[styles.text, { fontSize: 24 }]}>302</Text>
-                        <Text style={[styles.text, styles.subText]}>Following</Text>
-                    </View>
-                </View>
-
-                <View style={{ marginTop: 32 }}>
-                    <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("../assets/profile_media1.jpg")} style={styles.image} resizeMode="cover"></Image>
+                    <View style={{ alignSelf: "center" }}>
+                        <View style={styles.profileImage}>
+                            <Image source={require("../assets/profile_profile-pic.jpg")} style={styles.image} resizeMode="center"></Image>
                         </View>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("../assets/profile_media2.jpg")} style={styles.image} resizeMode="cover"></Image>
-                        </View>
-                        <View style={styles.mediaImageContainer}>
-                            <Image source={require("../assets/profile_media3.jpg")} style={styles.image} resizeMode="cover"></Image>
-                        </View>
-                    </ScrollView>
-                    <View style={styles.mediaCount}>
-                        <Text style={[styles.text, { fontSize: 24, color: "#DFD8C8", fontWeight: "300" }]}>70</Text>
-                        <Text style={[styles.text, { fontSize: 12, color: "#DFD8C8", textTransform: "uppercase" }]}>Media</Text>
-                    </View>
-                </View>
-                <Text style={[styles.subText, styles.recent]}>Recent Activity</Text>
-                <View style={{ alignItems: "center" }}>
-                    <View style={styles.recentItem}>
-                        <View style={styles.activityIndicator}></View>
-                        <View style={{ width: 250 }}>
-                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
-                                Started following <Text style={{ fontWeight: "400" }}>Jake Challeahe</Text> and <Text style={{ fontWeight: "400" }}>Luis Poteer</Text>
-                            </Text>
+                        <View style={styles.active}></View>
+                        <View style={styles.add}>
+                            <Ionicons name="ios-add" size={48} color="#DFD8C8" style={{ marginTop: 6, marginLeft: 2 }}></Ionicons>
                         </View>
                     </View>
 
-                    <View style={styles.recentItem}>
-                        <View style={styles.activityIndicator}></View>
-                        <View style={{ width: 250 }}>
-                            <Text style={[styles.text, { color: "#41444B", fontWeight: "300" }]}>
-                                Started following <Text style={{ fontWeight: "400" }}>Luke Harper</Text>
-                            </Text>
-                        </View>
+                    <View style={styles.infoContainer}>
+                        <Text style={[styles.text, { fontWeight: "200", fontSize: 36 }]}>{this.props.full_name}</Text>
                     </View>
-                </View>
-            </ScrollView>
-        </SafeAreaView>
-    );
+
+                    <View style={styles.statsContainer}>
+                        <View style={styles.statsBox}>
+                            <Text style={[styles.text, { fontSize: 24 }]}>0</Text>
+                            <Text style={[styles.text, styles.subText]}>Pins</Text>
+                        </View>
+                        <View style={[styles.statsBox, { borderColor: "#DFD8C8", borderLeftWidth: 1, borderRightWidth: 1 }]}>
+                            <Text style={[styles.text, { fontSize: 24 }]}>0</Text>
+                            <Text style={[styles.text, styles.subText]}>Reputation</Text>
+                        </View>
+                        
+                    </View>
+
+                    <View style={{ marginTop: 32 }}>
+                        <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
+                            <View style={styles.mediaImageContainer}>
+                                <Image source={require("../assets/profile_media1.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            </View>
+                            <View style={styles.mediaImageContainer}>
+                                <Image source={require("../assets/profile_media2.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            </View>
+                            <View style={styles.mediaImageContainer}>
+                                <Image source={require("../assets/profile_media3.jpg")} style={styles.image} resizeMode="cover"></Image>
+                            </View>
+                        </ScrollView>
+
+                    </View>
+                   
+                </ScrollView>
+            </SafeAreaView>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
